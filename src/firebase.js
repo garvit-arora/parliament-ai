4e25fdef0d1b962087f3094b56a4311717247552
Garvit Arora <garvit.university@gmail.com>
2026-04-14 13:32:29 +0530

    Removal : Updated hardcoded links

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpLan2C7iEXUUk4dIJwF1J4FEr4uuybQ0",
-  authDomain: "parliament-system.firebaseapp.com",
-  projectId: "parliament-system",
-  storageBucket: "parliament-system.firebasestorage.app",
-  messagingSenderId: "909013646955",
-  appId: "1:909013646955:web:eda7d890865da17d517b2e",
-  measurementId: "G-W1SRJ93XJS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
